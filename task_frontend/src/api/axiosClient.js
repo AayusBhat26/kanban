a5cab2e3a1e0ceef8dbe95d4dd627a329df9ff49
Aayush Kumar Bhat <kumarbhataayush@gmail.com>
2024-08-26 00:57:19 +0530

    base url

--- task_frontend/src/api/axiosClient.js
+++ task_frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'https://kanbanserver-five.vercel.app/api/v1'
+const baseUrl = 'https://kanbanclient.vercel.app/api/v1'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
